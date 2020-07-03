b1702b4970efcd3967a97d5268d0b61bda275dbf
Quang Truong <quang.truong@nois.vn>
2020-07-03 08:11:34 +0700

    commit

---
+++ DoiBong/DoiBong/Entities/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Player
+    {
+        public string  PlayerId { get; set; }
+        public string PlayerName { get; set; }
+        public string NumberPhone { get; set; }
+        public string Email { get; set; }
+        public string TeamId { get; set; }
+        public Team Team { get; set; }
+    }
+}
---
+++ DoiBong/DoiBong/Entities/Team.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Team
+    {
+        public string TeamId { get; set; }
+        public string TeamName { get; set; }
+        public ICollection<Player> Players { get; set; }
+    }
+}
